0d636127b0e7c39fae9c921528cfd499fa0b2718
eshikvtumane <eshikvtumanebest@gmail.com>
2015-02-12 22:03:29 +1000

    Add scripts (bootstrap, jquery, etc)

---
+++ static/js/work_with_div.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    //http://xdsoft.net/jqplugins/datetimepicker/
+    $('#datetimepicker').datetimepicker({
+        lang:'ru',
+        /*allowTimes:[
+          '12:00', '13:00', '15:00',
+          '17:00', '17:05', '17:20', '19:00', '20:00'
+         ]*/
+    });
+
+    $('#btn_next').click(function(){
+        console.log('OK x 1');
+        $('#email').hide();
+        $('#setting').show();
+        console.log('OK x 2');
+    });
+
+    $('#btn_back').click(function(){
+        console.log('OK x 1');
+        $('#setting').hide();
+        $('#email').show();
+        console.log('OK x 2');
+    });
+
+});
